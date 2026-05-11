b8ab5ba8582f13dbdb32f314c9b67beacd19fd5b
ibiinn <ibiinn05@gmail.com>
2026-05-11 19:54:13 +0700

    Lokasi Penyedia Makanan

---
+++ Tugas-CLO-2/Helpers/ValidationHelper.cs
@@ -0,0 +1,18 @@
+﻿namespace Tugas_CLO_2.Helpers
+{
+    public static class ValidationHelper
+    {
+        public static void ValidateCoordinate(double latitude, double longitude)
+        {
+            if (latitude < -90 || latitude > 90)
+            {
+                throw new ArgumentException("Latitude tidak valid");
+            }
+
+            if (longitude < -180 || longitude > 180)
+            {
+                throw new ArgumentException("Longitude tidak valid");
+            }
+        }
+    }
+}
---
+++ Tugas-CLO-2/Models/Store.cs
@@ -0,0 +1,11 @@
+﻿namespace Tugas_CLO_2.Models
+{
+    public class Store
+    {
+        public string Name { get; set; }
+
+        public double Latitude { get; set; }
+
+        public double Longitude { get; set; }
+    }
+}
