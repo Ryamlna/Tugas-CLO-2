5ad03b6281bc9c00054e91d937b012b243f4ff02
Ridho941 <firdausridho595@gmail.com>
2026-06-22 19:39:25 +0700

    Commit finish GUI Login dan Register

---
+++ TubesKPL2/Home.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace TubesKPL2
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label2 = new Label();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 28);
+            label2.Name = "label2";
+            label2.Size = new Size(200, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Selamat Datang di GoodBite";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 57);
+            label3.Name = "label3";
+            label3.Size = new Size(141, 20);
+            label3.TabIndex = 2;
+            label3.Text = "Anda Berhasil Login";
+            // 
+            // Home
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(439, 212);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Name = "Home";
+            Text = "Home";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Label label2;
+        private Label label3;
+    }
+}
