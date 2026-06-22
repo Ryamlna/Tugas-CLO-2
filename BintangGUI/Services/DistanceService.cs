a1d23403d01132d18f08a630dd6ff5a10fa0d6e3
ibiinn <ibiinn05@gmail.com>
2026-06-22 23:56:08 +0700

    update gui lokasi pencarian makanan

---
+++ BintangGUI/Services/DistanceService.cs
@@ -0,0 +1,33 @@
+namespace BintangGUI.Services
+{
+    public static class DistanceService
+    {
+        public static double CalculateDistance(
+            double lat1,
+            double lon1,
+            double lat2,
+            double lon2)
+        {
+            double R = 6371;
+
+            double dLat = ToRadians(lat2 - lat1);
+            double dLon = ToRadians(lon2 - lon1);
+
+            double a =
+                Math.Sin(dLat / 2) * Math.Sin(dLat / 2) +
+                Math.Cos(ToRadians(lat1)) *
+                Math.Cos(ToRadians(lat2)) *
+                Math.Sin(dLon / 2) *
+                Math.Sin(dLon / 2);
+
+            double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+
+            return R * c;
+        }
+
+        private static double ToRadians(double angle)
+        {
+            return angle * Math.PI / 180;
+        }
+    }
+}
