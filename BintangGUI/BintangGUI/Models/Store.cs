c39d777f91f1b0322a8d3639a9fc428f3b8439b5
ibiinn <ibiinn05@gmail.com>
2026-06-22 22:42:40 +0700

    Delete BintangGUI directory

--- BintangGUI/BintangGUI/Models/Store.cs
+++
@@ -1,9 +0,0 @@
-namespace BintangGUI.Models
-{
-    public class Store
-    {
-        public string Name { get; set; } = string.Empty;
-        public double Latitude { get; set; }
-        public double Longitude { get; set; }
-    }
-}
